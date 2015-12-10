05cbb6d0131fa6cd0702f1dc86636d095f5b9f59
Marco Vidal García <marcovidal67@gmail.com>
2015-12-10 22:24:12 +0100

    Clouds background and labels

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -1,9 +1,5 @@
 $(function() {
-  console.log('Hola');
-  var el =  document.createElement("h1");
-  el.id="title";
-  el.innerHTML = "Some title";
-  //document.body.appendChild(el);
+  console.log('poptheballoon!');
 });
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJidW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKGZ1bmN0aW9uKCkge1xuICBjb25zb2xlLmxvZygnSG9sYScpO1xuICB2YXIgZWwgPSAgZG9jdW1lbnQuY3JlYXRlRWxlbWVudChcImgxXCIpO1xuICBlbC5pZD1cInRpdGxlXCI7XG4gIGVsLmlubmVySFRNTCA9IFwiU29tZSB0aXRsZVwiO1xuICAvL2RvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQoZWwpO1xufSk7XG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYnVuZGxlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiJChmdW5jdGlvbigpIHtcbiAgY29uc29sZS5sb2coJ3BvcHRoZWJhbGxvb24hJyk7XG59KTtcbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,3 @@
 $(function() {
-  console.log('Hola');
-  var el =  document.createElement("h1");
-  el.id="title";
-  el.innerHTML = "Some title";
-  //document.body.appendChild(el);
+  console.log('poptheballoon!');
 });
